69f6aa5a4c5fb2402eda1275b9606117b253bd64
ebpearls <binod.chakradhar@ebpearls.com>
2025-02-17 17:45:17 +0545

    div structure updates

---
+++ assets/js/datepicker.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const filterForm = document.querySelector('.filter-wrap');
+    if (filterForm) {
+        let selectedStartDate = null;
+        let selectedEndDate = null;
+
+        const startDatePicker = flatpickr("#startDate", {
+            mode: "multiple", // Show two months but allow only one selection
+            dateFormat: "d M",
+            showMonths: 2,
+            minDate: "today",
+            onChange: function (selectedDates, dateStr, instance) {
+                if (selectedDates.length > 1) {
+                    instance.clear(); // Clear previous selection
+                    instance.setDate(selectedDates.slice(-1)); // Keep only the last selected date
+                }
+
+                selectedStartDate = selectedDates[0] || null;
+
+                if (selectedStartDate) {
+                    // Disable dates before the selected start date in the end date picker
+                    endDatePicker.set('minDate', selectedStartDate);
+                }
+
+                // Prevent selecting earlier dates in the start date picker after choosing a date
+                startDatePicker.set('disable', [(date) => date < selectedStartDate]);
+            }
+        });
+
+        const endDatePicker = flatpickr("#endDate", {
+            mode: "multiple", // Show two months but allow only one selection
+            dateFormat: "d M",
+            showMonths: 2,
+            onChange: function (selectedDates, dateStr, instance) {
+                if (selectedDates.length > 1) {
+                    instance.clear(); // Clear previous selection
+                    instance.setDate(selectedDates.slice(-1)); // Keep only the last selected date
+                }
+
+                selectedEndDate = selectedDates[0] || null;
+            }
+        });
+    }
+});
--- resources/enqueue-scripts.php
+++ resources/enqueue-scripts.php
@@ -11,6 +11,54 @@ function yacht_manager_enqueue_public_assets() {
         [],
         YACHT_MANAGER_VERSION
     );
+    wp_enqueue_style(
+        'yacht-manager-search-filter',
+        plugin_dir_url(__FILE__) . '../assets/css/search-filter.css',
+        [],
+        YACHT_MANAGER_VERSION
+    );
+    wp_enqueue_style(
+        'yacht-manager-filter-results',
+        plugin_dir_url(__FILE__) . '../assets/css/filter-results.css',
+        [],
+        YACHT_MANAGER_VERSION
+    );
+    wp_enqueue_style(
+        'yacht-manager-btstap-styl',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+        [],
+        YACHT_MANAGER_VERSION
+    );
+    wp_enqueue_style(
+        'yacht-manager-flatpickr-styl',
+        'https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.13/flatpickr.min.css',
+        [],
+        YACHT_MANAGER_VERSION
+    );
+    wp_enqueue_script(
+        'yacht-manager-btstap-script',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
+        [],
+        YACHT_MANAGER_VERSION
+    ); 
+    wp_enqueue_script(
+        'yacht-manager-flatpickr-script',
+        'https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.13/flatpickr.min.js',
+        [],
+        YACHT_MANAGER_VERSION
+    ); 
+    wp_enqueue_script(
+        'yacht-manager-datepicker',
+        plugin_dir_url(__FILE__) . '../assets/js/datepicker.js',
+        [],
+        YACHT_MANAGER_VERSION
+    ); 
+    wp_enqueue_script(
+        'yacht-manager-homepagefilter',
+        plugin_dir_url(__FILE__) . '../assets/js/form-input-filter.js',
+        [],
+        YACHT_MANAGER_VERSION
+    ); 
 }
 add_action('wp_enqueue_scripts', 'yacht_manager_enqueue_public_assets');
 
@@ -18,14 +66,12 @@ add_action('wp_enqueue_scripts', 'yacht_manager_enqueue_public_assets');
 function yacht_manager_enqueue_admin_assets() {
     global $post_type;
     if (isset($_GET['page']) && $_GET['page'] === 'yacht-manager') {
-
         wp_enqueue_style(
             'yacht-manager-admin',
             plugin_dir_url(__FILE__) . '../assets/css/admin-style.css',
             [],
             YACHT_MANAGER_VERSION
         );
-
         wp_enqueue_script(
             'yacht-manager-admin-menu-input',
             plugin_dir_url(__FILE__) . '../assets/js/admin-menu-input.js',
