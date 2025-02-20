71dad3f92afa2d9e4f12f8f3c29c78437a3db10d
ebpearls <binod.chakradhar@ebpearls.com>
2025-02-20 17:35:52 +0545

    Advanced filter choose field update

--- assets/js/advanced-filter-modal.js
+++ assets/js/advanced-filter-modal.js
@@ -14,6 +14,25 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-function ytmUpdateRangeValue(value) {
-  document.getElementById("rangeValue").textContent = value;
+/* range slider */
+  function ytmUpdateRangeValue(value) {
+    document.getElementById("rangeValue").textContent = value;
+    document.getElementById("ytm-cabin").value = value;
 }
+
+/* yacht type checkboxes */
+document.addEventListener("DOMContentLoaded", function () {
+  const checkboxes = document.querySelectorAll(".yacht-checkbox-ct");
+  const hiddenInput = document.getElementById("ytm-charter-type");
+
+  checkboxes.forEach((checkbox) => {
+      checkbox.addEventListener("change", function () {
+          let selectedValues = Array.from(checkboxes)
+              .filter((cb) => cb.checked)
+              .map((cb) => cb.value)
+              .join(","); // Join selected values as a comma-separated string
+          
+          hiddenInput.value = selectedValues;
+      });
+  });
+});
