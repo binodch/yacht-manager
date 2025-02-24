234fa6774c9d82b0c3713af417a1831995693c08
binod.chakradhar <binod.chakradhar@ebpearls.com>
2025-02-24 22:43:40 +0545

    Yacht post type single post template added

--- page-template/register-page-template.php
+++ page-template/register-page-template.php
@@ -18,3 +18,14 @@ function yacht_manager_load_page_template($template) {
     }
     return $template;
 }
+
+// Load the single yacht post template
+function mu_yacht_template($single) {
+    global $post;
+    if ($post->post_type == 'yacht') {
+        return plugin_dir_path(__FILE__) . 'single-yacht.php';
+    }
+
+    return $single;
+}
+add_filter('single_template', 'mu_yacht_template');
