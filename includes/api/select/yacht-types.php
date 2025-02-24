c5dc0c7b049816e831846a5aef20608634642ab2
ebpearls <binod.chakradhar@ebpearls.com>
2025-02-24 17:51:51 +0545

    Advanced filter functionallity update

--- includes/api/select/yacht-types.php
+++ includes/api/select/yacht-types.php
@@ -1,7 +1,6 @@
 <?php
 // yacht types
 function yacht_manager_curl_yacht_types() {
-    // $types = array('Cataraman', 'Gulet', 'Sailing', 'Motor', 'Power Cataraman', 'Classic', 'Expedition', 'Sport fishing') ;
     $terms = get_terms([
         'taxonomy'   => 'yacht-type',
         'hide_empty' => true,
