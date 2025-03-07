9d6539b65104071868be86239639a56db00d526d
ebpearls <binod.chakradhar@ebpearls.com>
2025-03-07 15:43:35 +0545

    Block Grid search added

---
+++ blocks/grid-search/grid-block.js
@@ -0,0 +1,50 @@
+wp.blocks.registerBlockType('custom/grid-search-block', {
+    title: 'Grid Search Block',
+    icon: 'admin-appearance',
+    category: 'yacht-manager',
+
+    attributes: {
+        title: { type: 'string', default: '' },
+        description: { type: 'string', default: '' },
+    },
+
+    edit: function (props) {
+        function updateTitle(event) {
+            props.setAttributes({ title: event.target.value });
+        }
+
+        function updateDescription(event) {
+            props.setAttributes({ description: event.target.value });
+        }
+
+        return (
+            wp.element.createElement('div', { className: 'custom-featured-entity-block' },
+                wp.element.createElement('div', { className: 'featured-title-wrapper block-featured-label' },
+                    wp.element.createElement('label', { htmlFor: 'featured-title-element' }, 'Title'),
+                    wp.element.createElement('input', {
+                        id: 'featured-title-element',
+                        type: 'text',
+                        value: props.attributes.title,
+                        onChange: updateTitle,
+                        placeholder: 'Title'
+                    }),
+                ),
+                wp.element.createElement('div', { className: 'featured-textarea-wrapper' },
+                    wp.element.createElement('label', { htmlFor: 'featured-textarea-element' }, 'Description'), 
+                    wp.element.createElement('textarea', {
+                        id: 'featured-textarea-element',
+                        value: props.attributes.description,
+                        onChange: updateDescription,
+                        placeholder: 'Description',
+                        rows: 5
+                    })
+                )
+                
+            )
+        );
+    },
+
+    save: function () {
+        return null; // Server-side rendering only
+    }
+});
--- templates/register-page-template.php
+++ templates/register-page-template.php
@@ -1,24 +1,5 @@
 <?php
-// Hook to register the template
-add_filter('theme_page_templates', 'yacht_manager_custom_page_template');
-function yacht_manager_custom_page_template($templates) {
-    $templates['find-yacht.php'] = 'Find a Yacht';
-    return $templates;
-}
-
-// Load the template when selected
-add_filter('template_include', 'yacht_manager_load_page_template');
-function yacht_manager_load_page_template($template) {
-    if (is_page()) {
-        $meta = get_page_template_slug(get_queried_object_id());
-        if ($meta && $meta === 'find-yacht.php') {
-            return plugin_dir_path(__FILE__) . 'find-yacht.php';
-        }
-    }
-    return $template;
-}
-
-// Load the single yacht post template
+/* Load the single yacht post template */
 function mu_yacht_template($single) {
     global $post;
     if ($post->post_type == 'yacht') {
